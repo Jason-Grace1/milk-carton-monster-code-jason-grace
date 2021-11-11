d68d1a2cbee7992a811e47edcc855b21c20c81d3
Jason-Grace1 <92944114+Jason-Grace1@users.noreply.github.com>
2021-11-11 08:42:50 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,32 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    pins.servoWritePin(AnalogPin.P0, 0)
+    basic.pause(500)
+    pins.servoWritePin(AnalogPin.P0, 180)
+    basic.pause(500)
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(0)
+})
+let angle = 0
+let opened = 95
+let closed = 175
+basic.forever(function () {
+    led.plotBarGraph(
+    input.lightLevel(),
+    0
+    )
+    pins.servoWritePin(AnalogPin.P0, input.lightLevel())
+})
+basic.forever(function () {
+    led.plotBarGraph(
+    input.lightLevel(),
+    0
+    )
+    angle = pins.map(
+    input.lightLevel(),
+    0,
+    255,
+    closed,
+    opened
+    )
+})
